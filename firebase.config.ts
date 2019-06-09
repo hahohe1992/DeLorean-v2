21689dd7feebb42b30f1fc83d137f5949113fa34
hahohe1992 <hsuho.1992@gmail.com>
2019-06-09 12:01:47 +0800

    GDG WTM event trail

---
+++ firebase.config.ts
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBBc0PUXOyVdo6DZNG1dT-N5qxlv_qsj5k',
+  authDomain: 'iwd1-ae8e9.firebaseapp.com',
+  databaseURL: 'https://iwd1-ae8e9.firebaseio.com',
+  projectId: 'iwd1-ae8e9',
+  storageBucket: 'iwd1-ae8e9.appspot.com',
+  messagingSenderId: '<SENDER_ID>',
+  // Firebase node which your data will be stored (ex. 2017)
+  devfestYear: '2019',
+  // Your Google Maps API key
+  mapsKey: 'AIzaSyD129_8snIi_hKtT2Bb7GUI6Ajw3Rd5Q-c'
+};
